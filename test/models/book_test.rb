de30a573af9959009b94d3ebb3aa5bea841fe0ad
Renan Montenegro <renanigt@gmail.com>
2016-09-02 18:46:43 -0300

    Adding the first and simple test

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "book attributes must not be empty" do
+    book = Book.new
+    assert book.invalid?
+    assert book.errors[:title].any?
+    assert book.errors[:resume].any?
+    assert book.errors[:author].any?
+    assert book.errors[:status].any?
+    assert book.errors[:category].any?
+  end
 end
