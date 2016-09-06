3630e6209eb50edb49e1487c1286d7da050cd56c
Renan Montenegro <renanigt@gmail.com>
2016-09-06 01:15:28 -0300

    Adding tests for BooksController

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @book = books(:rails)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:books)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:book)
+  end
+
+  test "shold create a new book" do
+    assert_difference('Book.count') do
+      post :create, book: { title: "JavaScript Book", 
+        resume: "Resume of JavaScript book", author: "Jane Doe", 
+        status: "read", category: "technology" }
+    end
+  end
+
 end
