81ca3979280e23a97d3f79315b2b3f3b9856a4c0
Renan Montenegro <renanigt@gmail.com>
2016-09-16 18:26:15 -0300

    Integration test for action edit

--- test/integration/crud_book_test.rb
+++ test/integration/crud_book_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CrudBookTest < ActionDispatch::IntegrationTest
-  
+
+  fixtures :books
+
   test "create a book" do
     get "/books/new"
     assert_response :success
@@ -9,6 +11,19 @@ class CrudBookTest < ActionDispatch::IntegrationTest
     post "/books", book: { title: "JavaScript Book", 
         resume: "Resume of JavaScript book", author: "Jane Doe", 
         status: "read", category: "technology" }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "edit a book" do
+    @book = books(:rails)
+
+    get "/books/#{@book.id}/edit"
+    assert_response :success
+
+    put "/books/#{@book.id}", book: { title: "Elixir Book", 
+        resume: "Resume of Elixir book", author: "José Valim" }
     assert_response :redirect
     follow_redirect!
     assert_response :success
